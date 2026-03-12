2e1b0d8ff93abd6d1c4820940515959a8b7d0e46
HongChaeMin <ghdcoalss33@gmail.com>
2026-03-12 15:26:35 +0900

    feat: add token-garden formula

---
+++ Formula/token-garden.rb
@@ -0,0 +1,44 @@
+class TokenGarden < Formula
+  include Language::Python::Virtualenv
+
+  desc "LLM 토큰 사용량을 터미널 정원으로 시각화 — GitHub 잔디처럼"
+  homepage "https://github.com/HongChaeMin/token-garden"
+  url "https://github.com/HongChaeMin/token-garden/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "475adb1f6d992575f79fa72aeea271412d8d783922f23a9692834230dcdaed90"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
+    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/87/67/a37f6214d0e9fe57f6ae54b2956d550ca8365857f42a1ce0392bb21343d0/rich-13.9.4.tar.gz"
+    sha256 "439594978a49a09530cff7ebc4b5c7103ef57baf48d5ea3184f21d9a2befa098"
+  end
+
+  resource "markdown-it-py" do
+    url "https://files.pythonhosted.org/packages/38/71/3b932df36c1a044d397a1f92d1cf91ee0a503d91e470cbd670aa66b07ed0/markdown-it-py-3.0.0.tar.gz"
+    sha256 "e3f60a94fa066dc52ec76661e37c851cb232d92f9886b15cb560aaada2df8feb"
+  end
+
+  resource "mdurl" do
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+  end
+
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/8e/62/8336eff65bcbc8e4cb5d05b55faf041285951b6e80f33e2bff2024788f31/pygments-2.18.0.tar.gz"
+    sha256 "786ff802f32e91311bff3889f6e9a86e81505fe99f2735bb6d60ae0c5004f199"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system bin/"token-garden", "--help"
+  end
+end
